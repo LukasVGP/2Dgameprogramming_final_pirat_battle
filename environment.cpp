7a5cf3427bac68bfaddec83cd5bf1df29f0fcdc6
LukasVGP <152808843+LukasVGP@users.noreply.github.com>
2024-12-12 15:25:27 -0800

    ocean extende and sky removed
    ocean extende and sky removed

--- environment.cpp
+++ environment.cpp
@@ -1,36 +1,19 @@
 #include "environment.h"
 
 Environment::Environment() {
-    // Initialize clouds with slower speed
-    for (int i = 0; i < MAX_CLOUDS; i++) {
-        clouds[i].position = { (float)GetRandomValue(0, 1200), (float)GetRandomValue(50, 200) };
-        clouds[i].size = GetRandomValue(30, 60);
-
-        clouds[i].speed = GetRandomValue(1, 3) / 300.0f;
-    }
     time = 0;
 }
 
-
 void Environment::Draw() {
-    DrawSky();
     DrawOcean();
     DrawPirateIsland();
     DrawNavyIsland();
-    DrawClouds();
     time += GetFrameTime();
 }
 
-void Environment::DrawSky() {
-    Color skyTop = { 135, 206, 235, 255 };
-    Color skyBottom = { 173, 216, 230, 255 };
-    DrawRectangleGradientV(0, 0, 1200, 400, skyTop, skyBottom);
-    DrawCircle(1000, 100, 40, YELLOW);
-    DrawCircle(1000, 100, 35, GOLD);
-}
-
 void Environment::DrawOcean() {
-    for (int y = 400; y < 1200; y += 20) {
+    // Fill entire background with ocean
+    for (int y = 0; y < 1200; y += 20) {
         for (int x = 0; x < 1200; x += 40) {
             float wave = sinf((x + time * 50) / 100.0f) * 5;
             DrawLineEx(
@@ -59,14 +42,4 @@ void Environment::DrawNavyIsland() {
     DrawRectangle(985, 480, 30, 20, DARKBLUE);
     DrawRectangle(995, 480, 10, 20, RED);
     DrawRectangle(985, 485, 30, 10, RED);
-}
-
-void Environment::DrawClouds() {
-    for (int i = 0; i < MAX_CLOUDS; i++) {
-        clouds[i].position.x += clouds[i].speed;
-        if (clouds[i].position.x > 1200) clouds[i].position.x = -clouds[i].size;
-        DrawCircle(clouds[i].position.x, clouds[i].position.y, clouds[i].size, WHITE);
-        DrawCircle(clouds[i].position.x + clouds[i].size * 0.5f,
-            clouds[i].position.y, clouds[i].size * 0.8f, WHITE);
-    }
 }
--- environment.h
+++ environment.h
@@ -8,20 +8,8 @@ public:
     void Draw();
 
 private:
-    void DrawSky();
     void DrawOcean();
     void DrawPirateIsland();
     void DrawNavyIsland();
-    void DrawWaves();
-    void DrawClouds();
-
-    struct Cloud {
-        Vector2 position;
-        float size;
-        float speed;
-    };
-
-    static const int MAX_CLOUDS = 10;
-    Cloud clouds[MAX_CLOUDS];
     float time;
 };
