436cbfd317bc537dec1759ef26ecf7f5f3a3f70b
LukasVGP <152808843+LukasVGP@users.noreply.github.com>
2024-12-19 10:41:51 -0800

    uodate

--- menu.cpp
+++ menu.cpp
@@ -3,43 +3,93 @@
 Menu::Menu() {
     startButton = { 500, 500, 200, 60 };
     restartButton = { 500, 600, 200, 60 };
+    waveOffset = 0.0f;
+    flagSize = { FLAG_WIDTH, FLAG_HEIGHT };
+}
 
-    Image pirateImg = LoadImage("resources/pirate_flag.png");
-    Image navyImg = LoadImage("resources/navy_flag.png");
-    pirateFlag = LoadTextureFromImage(pirateImg);
-    navyFlag = LoadTextureFromImage(navyImg);
-    UnloadImage(pirateImg);
-    UnloadImage(navyImg);
+Menu::~Menu() {}
+
+void Menu::DrawWaves() {
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), DARKBLUE);
+
+    waveOffset += 0.5f;
+    for (int y = 0; y < GetScreenHeight(); y += 100) {
+        for (int x = -100; x < GetScreenWidth() + 100; x += 100) {
+            float yPos = y + sinf((x + waveOffset) * 0.05f) * 20;
+            DrawCircle(x, yPos, 50, ColorAlpha(BLUE, 0.3f));
+        }
+    }
+}
+
+void Menu::DrawPirateFlag(float x, float y) {
+    DrawRectangle(x, y, flagSize.x, flagSize.y, BLACK);
+
+    Vector2 center = { x + flagSize.x / 2, y + flagSize.y / 2 };
+    DrawCircle(center.x, center.y - 10, 20, WHITE);
+    DrawLineEx({ x + 20, y + flagSize.y - 20 },
+        { x + flagSize.x - 20, y + flagSize.y - 20 },
+        5, WHITE);
+}
+
+void Menu::DrawUnionJack(float x, float y) {
+    DrawRectangle(x, y, flagSize.x, flagSize.y, DARKBLUE);
+
+    DrawRectangle(x + flagSize.x / 2 - 5, y, 10, flagSize.y, WHITE);
+    DrawRectangle(x, y + flagSize.y / 2 - 5, flagSize.x, 10, WHITE);
+    DrawLineEx({ x, y }, { x + flagSize.x, y + flagSize.y }, 8, RED);
+    DrawLineEx({ x + flagSize.x, y }, { x, y + flagSize.y }, 8, RED);
 }
 
 void Menu::DrawTitle() {
     const char* title = "PIRATE HUNTER";
     int fontSize = 80;
     int textWidth = MeasureText(title, fontSize);
-    DrawText(title, 600 - textWidth / 2, 200, fontSize, GOLD);
+    int screenWidth = GetScreenWidth();
+    int centerX = screenWidth / 2;
+
+    DrawText(title, centerX - textWidth / 2 + 4, 204, fontSize, BLACK);
+
+    for (int i = 0; i < strlen(title); i++) {
+        char c[2] = { title[i], '\0' };
+        Color letterColor = ColorFromHSV(static_cast<float>(i * 15), 0.7f, 1.0f);
+        Vector2 position = {
+            static_cast<float>(centerX - textWidth / 2 + MeasureText(title, fontSize) / strlen(title) * i),
+            200.0f + sinf(GetTime() * 2 + i) * 5
+        };
+        DrawTextEx(GetFontDefault(), c, position, fontSize, 2, letterColor);
+    }
 }
 
 void Menu::DrawFlags() {
-    DrawTexture(pirateFlag, 300, 300, WHITE);
-    DrawTexture(navyFlag, 800, 300, WHITE);
+    int screenWidth = GetScreenWidth();
+    int centerX = screenWidth / 2;
+    int titleWidth = MeasureText("PIRATE HUNTER", 80);
+
+    DrawPirateFlag(centerX - (titleWidth / 2) - FLAG_WIDTH - FLAG_SPACING, 180);
+    DrawUnionJack(centerX + (titleWidth / 2) + FLAG_SPACING, 180);
 }
 
 void Menu::Draw() {
+    DrawWaves();
     DrawTitle();
     DrawFlags();
 
-    DrawRectangleRounded(startButton, 0.2f, 8, DARKBLUE);
+    DrawRectangleRounded(startButton, 0.2f, 8, ColorAlpha(DARKBLUE, 0.8f));
+    DrawRectangleRoundedLines(startButton, 0.2f, 8, GOLD);
     DrawText("START", startButton.x + 50, startButton.y + 15, 40, WHITE);
 }
 
 void Menu::DrawGameOver(bool playerWon) {
+    DrawWaves();
     DrawTitle();
     DrawFlags();
 
     const char* result = playerWon ? "VICTORY!" : "DEFEAT!";
-    DrawText(result, 500, 400, 60, playerWon ? GREEN : RED);
+    int resultWidth = MeasureText(result, 60);
+    DrawText(result, GetScreenWidth() / 2 - resultWidth / 2, 400, 60, playerWon ? GREEN : RED);
 
-    DrawRectangleRounded(restartButton, 0.2f, 8, DARKBLUE);
+    DrawRectangleRounded(restartButton, 0.2f, 8, ColorAlpha(DARKBLUE, 0.8f));
+    DrawRectangleRoundedLines(restartButton, 0.2f, 8, GOLD);
     DrawText("RESTART", restartButton.x + 35, restartButton.y + 15, 40, WHITE);
 }
 
--- menu.h
+++ menu.h
@@ -1,19 +1,30 @@
 #pragma once
 #include "raylib.h"
+#include <cmath>
+#include <cstring>
 
 class Menu {
 public:
     Menu();
+    ~Menu();
     void Draw();
     bool IsStartPressed();
     void DrawGameOver(bool playerWon);
     bool IsRestartPressed();
 
 private:
+    static const int FLAG_WIDTH = 150;
+    static const int FLAG_HEIGHT = 100;
+    static const int FLAG_SPACING = 50;
+
     Rectangle startButton;
     Rectangle restartButton;
-    Texture2D pirateFlag;
-    Texture2D navyFlag;
+    float waveOffset;
+    Vector2 flagSize;
+
     void DrawTitle();
     void DrawFlags();
+    void DrawWaves();
+    void DrawPirateFlag(float x, float y);
+    void DrawUnionJack(float x, float y);
 };
